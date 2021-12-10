c7a60e3529e0d8e7c9d6bda9a80501dd470a13ac
j299079 <j299079>
2021-12-10 17:29:57 +0900

    # 54 修正済み！

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,19 +1,34 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { useHome } from "./useHome";
 
 type Props = {};
+/**
+ * home 画面の外観を定義する tsx ファイル
+ * @param props
+ * @returns
+ */
 const View: React.FC<Props> = (props) => {
   const { children, ...rest } = props;
+  const context = useHome();
   return (
+    // Gridのコンテナ属性のあるものが1つの箱になる
     <Grid container>
+      {/* 箱の中は12分割してレイアウトを構成する */}
       <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
         Hello
       </Grid>
-      <Grid item xs={4} sx ={{ backgroundColor: ""}}>
-        World
+      <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
+        Hello
       </Grid>
-      <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
-        ふくおかクラウドアライアンス
+      <Grid item xs={4} sx={{ backgroundColor: "" }}>
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
       </Grid>
     </Grid>
   );
