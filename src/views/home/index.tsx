450a5b8bfe586ee6fb4eadc6faeb6e9e64778d9e
hironab <hiro.622@gmail.com>
2021-12-10 20:12:08 +0900

    #52　ソースを修正

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,7 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 type Props = {};
 const View: React.FC<Props> = (props) => {
@@ -7,6 +9,13 @@ const View: React.FC<Props> = (props) => {
   return (
     <Grid container>
       <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
+
+      <Stack spacing={2} direction="row">
+      <Button variant="text">あ</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      
+    </Stack>
         Hello
       </Grid>
       <Grid item xs={4} sx ={{ backgroundColor: ""}}>
