f21bd5259abe788986d7558e5b6f63be3229745b
Shunsuke Watanabe <s-watanabe@gbc.co.jp>
2021-12-10 17:56:17 +0900

    #59 top画面編集

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,16 +1,39 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import { useHome } from './useHome';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 type Props = {};
+
+/**
+ * home 画面の外観を定義する　tsxファイル
+ * @param props 
+ * @returns 
+ */
 const View: React.FC<Props> = (props) => {
   const { children, ...rest } = props;
+  const context = useHome();
   return (
+    //Grid のコンテナー属性のあるものが、１つの箱となる。
     <Grid container>
-      <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
+      {/** 12分割してレイアウトを構成する **/}
+      <Grid item xs={6} sx={{ backgroundColor: "#F2987D" }}>
         Hello
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
       </Grid>
-      <Grid item xs={4} sx ={{ backgroundColor: ""}}>
+      <Grid item xs={6} sx ={{ backgroundColor: ""}}>
         World
+        <Checkbox {...label} defaultChecked />
+        <Checkbox {...label} />
+        <Checkbox {...label} disabled />
+        <Checkbox {...label} disabled checked />
       </Grid>
       <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
         ふくおかクラウドアライアンス
