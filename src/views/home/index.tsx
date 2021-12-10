1d0afef4d8a1986e4562a6c0472eb1cf6043c6cf
kamikawatoko <kamikawatoko.wataru@adniss.jp>
2021-12-10 17:30:13 +0900

    #44 初めましてreactのtsxファイルを修正して、チェックインする

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,23 @@
 import { Grid } from "@mui/material";
 import React from "react";
 
+import { useHome } from "./useHome";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 type Props = {};
+/**
+ * home 画面の外観を定義するtsxファイル
+ */
 const View: React.FC<Props> = (props) => {
   const { children, ...rest } = props;
+  const context = useHome();
   return (
+    // Grid のコンテナ属性のあるものが一つの箱になる
     <Grid container>
+      {/** 箱の中は12分割してレイアウトを構成する 
+       * item xs={2} 一番小さいレイアウトサイズでも２分割
+      */}
       <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
         Hello
       </Grid>
@@ -15,8 +27,25 @@ const View: React.FC<Props> = (props) => {
       <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
         ふくおかクラウドアライアンス
       </Grid>
+      <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
+        workspace1
+      </Grid>
+      <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
+        workspace2
+      </Grid>
+      <Grid item xs={4} sx ={{ backgroundColor: "#C1DBDA"}}>
+      <Stack direction="row" spacing={2}>
+      <Button color="secondary">Secondary</Button>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
+    </Stack>
+      </Grid>
     </Grid>
-  );
+);
 };
 
 export default View;
---
+++ src/views/home/useHome.ts
@@ -0,0 +1,6 @@
+/** 
+ * home用のtsxの振る舞いを実装するカスタムhooks関数
+*/
+export function useHome(){
+
+}
