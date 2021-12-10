43d36dabcbea65e88e910dfdf08d120d99a328d0
yazawa8 <yaza_ta@yahoo.co.jp>
2021-12-10 17:30:26 +0900

    #53 これからチェックインを行います

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,24 @@
+/** 画面の外観を定義する tsxファイル */
 import { Grid } from "@mui/material";
 import React from "react";
+import { useHome } from "./useHome";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 type Props = {};
 const View: React.FC<Props> = (props) => {
   const { children, ...rest } = props;
+  const context = useHome(); 
   return (
+    // Gridのcontainer属性で箱を作る
     <Grid container>
+      {/* 箱の中を12分割してレイアウトを構成する */}
       <Grid item xs={4} sx={{ backgroundColor: "#F2987D" }}>
-        Hello
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
       </Grid>
       <Grid item xs={4} sx ={{ backgroundColor: ""}}>
         World
