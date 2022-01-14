ecbd19fcc40fe2b82ba051ee5f7fa4d4f663d31a
satoki hayashi <hayashi@addspace-fuk.com>
2022-01-14 15:51:36 +0900

    サンプルの実装

--- src/views/sample11/useSample11.ts
+++ src/views/sample11/useSample11.ts
@@ -1,1 +1,18 @@
-export function useSample11() {}
+export function useSample11() {
+
+    const action1 = () => {
+        alert("HELLOボタンが押下されました！");
+      };
+
+
+    const action2 = () => {
+        alert("WORLDボタンが押下されました！");
+      };
+
+
+    const action3 = () => {
+        alert("HELLO WORLDボタンが押下されました！");
+      };
+
+    return { action1, action2,  action3 };
+}
