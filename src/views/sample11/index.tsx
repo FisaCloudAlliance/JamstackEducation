37f833e45852316ded7515f52b27bae26c2e0e04
hironab <hiro.622@gmail.com>
2022-01-14 15:49:48 +0900

    サンプルの実装

--- src/views/sample11/index.tsx
+++ src/views/sample11/index.tsx
@@ -8,15 +8,21 @@ const Sample11: React.FC<Props> = (props) => {
   return (
     <Grid container>
       <Grid item xs={12} sm={6} md={4}>
-        <Button variant="contained">Hello</Button>
+        <Button variant="contained" 
+                onClick={() => context.action1()} >
+                Contained 
+        Hello</Button>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
-        <Button variant="text">World</Button>
+      <Button variant="text" 
+                onClick={() => context.action2()} >
+                Contained 
+                World</Button>
       </Grid>
 
       <Grid item xs={12} sm={6} md={4}>
-        <Button variant="outlined">Heelo World!</Button>
+        <Button variant="outlined"　onClick={() => context.sampleAction()} >Contained　Heelo World!</Button>
       </Grid>
     </Grid>
   );
